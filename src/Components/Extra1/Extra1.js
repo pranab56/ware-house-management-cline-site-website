a6782b6cb8cafd3d6f0444a14595a8598c86eb45
pranab56 <pronabhalder53@gmail.com>
2022-05-09 15:11:10 +0600

    collection done

--- src/Components/Extra1/Extra1.js
+++ src/Components/Extra1/Extra1.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Extra1.css'
-import all from '../../Images/Beauty-Fruits.png'
+import all from '../../Images/fruits-3.png'
 import juicy from '../../Images/Juicy-Fruits.png'
 import beauty from '../../Images/Beauty-Fruits.png'
 
@@ -10,9 +10,9 @@ const Extra1 = () => {
         <div className="ms-5 me-5 mt-5 me-4">
             <div className="row g-3">
                 <div className="col-lg-4 col-md-6">
-                    <div className="d-flex p-2 justify-content-between align-items-center color">
+                    <div className="d-flex mt-4 p-2 justify-content-between align-items-center color">
                         <h2>All Fruits Collection</h2>
-                        <img className='img-fluid' src={all} alt="" />
+                        <img style={{width : '100px' }} className='img-fluid' src={all} alt="" />
                     </div>
                 </div>
                 <div className="col-lg-4 col-md-6">
