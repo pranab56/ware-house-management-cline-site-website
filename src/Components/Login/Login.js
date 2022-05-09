cafc445834f0d2e9a764b9fb7dcf1df9ed0b4a38
pranab56 <pronabhalder53@gmail.com>
2022-05-09 19:22:14 +0600

    login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -22,7 +22,7 @@ const Login = () => {
   const location = useLocation();
   let from = location.state?.from?.pathname || "/";
 
-  const [signInWithEmailAndPassword, user, loading, error] =
+  const [signInWithEmailAndPassword, loading, error] =
     useSignInWithEmailAndPassword(auth);
 
   const handleLogin = async (event) => {
